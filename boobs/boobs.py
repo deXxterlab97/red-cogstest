705e85be9e663ea4788d2792957a1e476d9c92d7
Jack <31835797+deXxterlab97@users.noreply.github.com>
2025-02-08 21:45:52 +0700

    Add files via upload

--- boobs/boobs.py
+++ boobs/boobs.py
@@ -1,17 +1,19 @@
 import discord
 import random
+import datetime
+from time import gmtime, strftime
 from redbot.core import commands
 from redbot.core.utils.chat_formatting import pagify
 
 
 class Boobs(commands.Cog):
-    """Penis related commands."""
+    """Boobies related commands."""
 
     @commands.command()
     async def boobscompare(self, ctx, *users: discord.Member):
         """Detects user's breast size
 
-        This is 110% accurate. 
+        This is 110% accurate and have been tested on males and females.
         Enter multiple users for an accurate comparison!"""
         if not users:
             await ctx.send_help()
@@ -20,9 +22,10 @@ class Boobs(commands.Cog):
         boobs = {}
         msg = ""
         state = random.getstate()
-
+        time = strftime("%-H",gmtime())
+        fulltime = strftime("%I%p",gmtime())
         for user in users:
-            
+            random.seed(str(time))
 
             if ctx.bot.user.id == user.id:
                 length = 50
@@ -35,7 +38,8 @@ class Boobs(commands.Cog):
         boobs = sorted(boobs.items(), key=lambda x: x[1])
 
         for user, boob in boobs:
-            msg += "**{}'s size:**\n{}\n".format(user.display_name, boob)
+            msg += "**{}'s size at {}:**\n{}\n".format(user.display_name, fulltime, boob)
 
         for page in pagify(msg):
             await ctx.send(page)
+            
