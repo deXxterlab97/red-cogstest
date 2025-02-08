2a75eb991e3b929eb0950065fcde46d66169f37f
Jack <31835797+deXxterlab97@users.noreply.github.com>
2025-02-09 00:39:37 +0700

    Update penis.py

--- penis/penis.py
+++ penis/penis.py
@@ -27,7 +27,7 @@ class Penis(commands.Cog):
         
         for user in users:
             time = int(time) + int(user.id)
-            random.seed(str(time))
+            random.seed(time)
 
             if ctx.bot.user.id == user.id:
                 length = 50
