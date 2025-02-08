59eaa6796675ea5dcaec32bce6d6f578912ba71d
Jack <31835797+deXxterlab97@users.noreply.github.com>
2025-02-08 23:22:36 +0700

    Add files via upload

---
+++ penis/penis.py
@@ -0,0 +1,43 @@
+import discord
+import random
+from redbot.core import commands
+from redbot.core.utils.chat_formatting import pagify
+
+
+class Penis(commands.Cog):
+    """Penis related commands."""
+
+    @commands.command()
+    async def penis(self, ctx, *users: discord.Member):
+        """Detects user's penis length
+
+        This is 100% accurate.
+        Enter multiple users for an accurate comparison!"""
+        if not users:
+            await ctx.send_help()
+            return
+
+        dongs = {}
+        msg = ""
+        state = random.getstate()
+        time = strftime("%-H",gmtime())
+        fulltime = strftime("%I%p",gmtime())
+        
+        for user in users:
+            random.seed(str(time))
+
+            if ctx.bot.user.id == user.id:
+                length = 50
+            else:
+                length = random.randint(0, 30)
+
+            dongs[user] = "8{}D".format("=" * length)
+
+        random.setstate(state)
+        dongs = sorted(dongs.items(), key=lambda x: x[1])
+
+        for user, dong in dongs:
+            msg += "**{}'s size at {}:**\n{}\n".format(user.display_name, fulltime, dong)
+
+        for page in pagify(msg):
+            await ctx.send(page)
