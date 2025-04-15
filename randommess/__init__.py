e344e589c88aee66aa65d3f1b75a9ebcde916725
deXxterlab97 <dexterlab977@gmail.com>
2025-04-15 20:59:32 +0700

    fixed imports

--- randommess/__init__.py
+++ randommess/__init__.py
@@ -1,6 +1,9 @@
 import copy
 import random
 
+import discord
+from discord.ext import tasks
+from redbot.core.bot import Red
 from redbot.core import commands
 
 file = open('/home/dex/output101.csv', 'r+')
