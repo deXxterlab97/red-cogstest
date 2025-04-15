745e417bad0b1cb852eb8e49d851a52d6bb42181
deXxterlab97 <dexterlab977@gmail.com>
2025-04-15 22:09:12 +0700

    Update __init__.py

--- randommess/__init__.py
+++ randommess/__init__.py
@@ -15,10 +15,15 @@ MESSAGE = MESSAGE.splitlines()
 class RandomMess(commands.Cog):
     @commands.command()
     async def randommess(self, ctx):
-        global message
+        global message1
         message = str(random.choice(MESSAGE))
-        await ctx.send(message)
-    
+        message1 = message.split(,)
+        msg = message1[0]
+        await ctx.send(msg)
+    @commands.command()
+    async def randomans()
+        msg2 = message1[1]
+        await ctx.send(msg2)
 
 async def setup(bot):
     await bot.add_cog(RandomMess())
