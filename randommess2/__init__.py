9063e4cd894dcd6ff5f7d78c276e6f3d200208fa
deXxterlab97 <dexterlab977@gmail.com>
2025-04-16 22:53:24 +0700

    test with tab

--- randommess2/__init__.py
+++ randommess2/__init__.py
@@ -6,7 +6,7 @@ from discord.ext import tasks
 from redbot.core.bot import Red
 from redbot.core import commands
 
-file = open('/home/dex/endr-yensign.csv', 'r+')
+file = open('/home/dex/end-tab.txt', 'r+')
 
 MESSAGE = file.read()
 
@@ -17,7 +17,7 @@ class RandomMess2(commands.Cog):
     async def randommess(self, ctx):
         global message1
         message = str(random.choice(MESSAGE))
-        message1 = message.split("¥")
+        message1 = message.split("\t")
         msg = message1[0]
         await ctx.send(msg)
     @commands.command()
