0547ba1781fcc2d3156e41dc2297b548d0b43e57
deXxterlab97 <dexterlab977@gmail.com>
2025-04-16 19:14:43 +0700

    Create __init__.py

---
+++ randommess2/__init__.py
@@ -0,0 +1,29 @@
+import copy
+import random
+
+import discord
+from discord.ext import tasks
+from redbot.core.bot import Red
+from redbot.core import commands
+
+file = open('/home/dex/test2.csv', 'r+')
+
+MESSAGE = file.read()
+
+MESSAGE = MESSAGE.splitlines()
+
+class RandomMess(commands.Cog):
+    @commands.command()
+    async def randommess(self, ctx):
+        global message1
+        message = str(random.choice(MESSAGE))
+        message1 = message.split(";")
+        msg = message1[0]
+        await ctx.send(msg)
+    @commands.command()
+    async def randomans(self, ctx):
+        msg2 = message1[1]
+        await ctx.send(msg2)
+
+async def setup(bot):
+    await bot.add_cog(RandomMess())
