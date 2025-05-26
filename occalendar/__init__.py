0d04aad9a3c17b9360acb48943479bbf4c09c471
deXxterlab97 <dexterlab977@gmail.com>
2025-05-27 02:25:55 +0700

    test1

--- occalendar/__init__.py
+++ occalendar/__init__.py
@@ -11,14 +11,9 @@ class occalendar(commands.Cog):
     """Calendar of important Owl City events."""
     @commands.command()
     async def ocreleases(self, ctx, filepath: "events"):
-        if filepath == 'current':
-            now = datetime.now()
-            month = now.strftime("%B")
-            with open(month + ".txt", "r") as fp:
-                await ctx.send(f.read())
-        else:        
-            with open(filepath + ".txt", "r") as fp:
-                await ctx.send(f.read())
+       
+        with open(filepath + ".txt", "r") as fp:
+             await ctx.send(f.read())
             
             
 
