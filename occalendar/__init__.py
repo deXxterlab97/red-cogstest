4711077cf42ff9c5de6fb87d5f37ced9252fb8fe
deXxterlab97 <dexterlab977@gmail.com>
2025-05-27 02:39:40 +0700

    Update __init__.py

--- occalendar/__init__.py
+++ occalendar/__init__.py
@@ -1,8 +1,6 @@
-import datetime
-from typing import Final, Optional, cast
+from datetime import datetime
 
 import discord
-from discord.ext import tasks
 from redbot.core.bot import Red
 from redbot.core import commands
 
@@ -10,7 +8,7 @@ from redbot.core import commands
 class occalendar(commands.Cog):
     """Calendar of important Owl City events."""
     @commands.command()
-    async def ocreleases(self, ctx, filepath=events):
+    async def ocreleases(self, ctx, filepath="events"):
        
         with open(filepath + ".txt", "r") as fp:
              await ctx.send(f.read())
