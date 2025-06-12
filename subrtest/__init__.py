5df07dcd5614f556cab0f7826bc534f85070f194
deXxterlab97 <dexterlab977@gmail.com>
2025-06-12 11:03:19 +0700

    Update __init__.py

--- subrtest/__init__.py
+++ subrtest/__init__.py
@@ -9,8 +9,7 @@ reddit = praw.Reddit(client_id='p4UNCgfF0qQW9XhglMJo5Q',
                      client_secret='YRJM-PB3nQwK7vPauxfbVIw8xf92zQ',
                      user_agent='discord')
 
-class subrtest(commands.Cog):
-    
+class subrtest(commands.Cog):    
     async def sub1():
         memes_submissions = reddit.subreddit('boobs').hot()
         post_to_pick = random.randint(1, 10)
@@ -20,4 +19,4 @@ class subrtest(commands.Cog):
         await bot.say(submission.url)
 
 async def setup(bot):
-    await bot.add_cog(subrtest(bot))
+    await bot.add_cog(subrtest())
