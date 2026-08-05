f9f925007e52d4c5c4a6ae66e2e4455f530c23d6
Jack <31835797+deXxterlab97@users.noreply.github.com>
2026-08-05 20:13:38 +0200

    Update __init__.py

--- memberroles/__init__.py
+++ memberroles/__init__.py
@@ -5,7 +5,7 @@ from redbot.core import commands
 
 class memberroles(commands.Cog):
     @commands.command()
-    async def memberroles(ctx):
+    async def memberroles(self, ctx):
         for member in ctx.message.server.members:
             if not get(member.roles, name='Verified Owl'):
                 await ctx.send(member)
