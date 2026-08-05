04a92fe621fcc9219e8f838f6b1d2452f24f4fa7
Jack <31835797+deXxterlab97@users.noreply.github.com>
2026-08-06 00:20:39 +0200

    Update __init__.py

--- memberroles/__init__.py
+++ memberroles/__init__.py
@@ -6,6 +6,8 @@ from redbot.core import commands
 class memberroles(commands.Cog):
     @commands.command()
     async def memberroles(self, ctx, role: discord.Role):
+        await ctx.send(role)
+        return
         for member in ctx.guild.members:
             if not member.bot and role not in member.roles:
                 await ctx.send(member.display_name)
