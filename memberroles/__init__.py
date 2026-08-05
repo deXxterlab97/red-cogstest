c76b80c8a7dba843abfc3b1ccee0ac4fc0afa43b
Jack <31835797+deXxterlab97@users.noreply.github.com>
2026-08-06 00:29:28 +0200

    Update __init__.py

--- memberroles/__init__.py
+++ memberroles/__init__.py
@@ -6,8 +6,10 @@ from redbot.core import commands
 class memberroles(commands.Cog):
     @commands.command()
     async def memberroles(self, ctx, role: discord.Role):
+        await ctx.send("Start processing")  
         for member in ctx.guild.members:
             if not member.bot and role not in member.roles:
                 await ctx.send(member.display_name)
+        await ctx.send("Finish processing")            
 async def setup(bot):
     await bot.add_cog(memberroles())
