7f4a0100758acedf097ed1f5ec035247c2c2ecd9
Jack <31835797+deXxterlab97@users.noreply.github.com>
2026-08-05 20:31:22 +0200

    Update __init__.py

--- memberroles/__init__.py
+++ memberroles/__init__.py
@@ -2,18 +2,21 @@ import discord
 from discord.ext import tasks
 from redbot.core.bot import Red
 from redbot.core import commands
+from discord.ext import commands
 
 class memberroles(commands.Cog):
     @commands.command()
     async def memberroles(self, ctx):
-        guild = guild=ctx.guild
-        for member in guild.members:
-            if not get(member.roles, name='Verified Owl'):
-                await ctx.send(member)
-    
+        role = ctx.guild.get_role(1533590480170778654)
+        members = [
+            member.display_name
+            for member in ctx.guild.members
+            if role not in member.roles
+        ]
+        await ctx.send("\n".join(members))
+
 async def setup(bot):
-    await bot.add_cog(memberroles())            
-            
+    await bot.add_cog(memberroles())  
     
 
 
