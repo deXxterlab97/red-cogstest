0d33ef0937897e1a4a2cafc0de73eed0fa4c498c
Jack <31835797+deXxterlab97@users.noreply.github.com>
2026-08-05 20:09:31 +0200

    Create __init__.py

---
+++ memberroles/__init__.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import tasks
+from redbot.core.bot import Red
+from redbot.core import commands
+
+class memberroles(commands.Cog):
+    @commands.command()
+    async def memberroles(ctx):
+    for member in ctx.message.server.members:
+        if not get(member.roles, name='Verified Owl'):
+            await ctx.send(member)
+    
+async def setup(bot):
+    await bot.add_cog(memberroles())            
+            
+    
+
+
