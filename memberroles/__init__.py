a90f91667465f3af3db7bb7771e099ce331801db
Jack <31835797+deXxterlab97@users.noreply.github.com>
2026-08-06 00:35:42 +0200

    Update __init__.py

--- memberroles/__init__.py
+++ memberroles/__init__.py
@@ -9,7 +9,7 @@ class memberroles(commands.Cog):
         await ctx.send("**Start processing**")  
         for member in ctx.guild.members:
             if not member.bot and role not in member.roles:
-                await ctx.send(f"{member.display_name} <@{member.id}>)
+                await ctx.send(f"{member.display_name} <@{member.id}>")
         await ctx.send("**Finish processing**")            
 async def setup(bot):
     await bot.add_cog(memberroles())
