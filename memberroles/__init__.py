84d0022ccb98f68778f0de2987accf1b9a7685d4
Jack <31835797+deXxterlab97@users.noreply.github.com>
2026-08-05 20:33:14 +0200

    Update __init__.py

--- memberroles/__init__.py
+++ memberroles/__init__.py
@@ -6,15 +6,14 @@ from discord.ext import commands
 
 class memberroles(commands.Cog):
     @commands.command()
-    async def memberroles(self, ctx):
-        role = ctx.guild.get_role(1533590480170778654)
+    async def memberroles(self, ctx, roleid):
+        role = ctx.guild.get_role(roleid)
         members = [
             member.display_name
             for member in ctx.guild.members
             if role not in member.roles
         ]
         await ctx.send("\n".join(members))
-
 async def setup(bot):
     await bot.add_cog(memberroles())  
     
